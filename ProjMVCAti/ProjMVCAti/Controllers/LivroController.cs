48033c555b62b7285c2502f7b191a501a1934614
brulogatti <bru.logatti@gmail.com>
2019-11-05 21:48:37 -0300

    05-11

---
+++ ProjMVCAti/ProjMVCAti/Controllers/LivroController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjMVCAti.Controllers
+{
+    public class LivroController : Controller
+    {
+        //GET: Livro
+        public ActionResult Index()
+        {
+            DadosEntities context = new DadosEntities();
+            var lst = context.tb_livro.ToList();
+            return View(lst);
+        }
+
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Cadastrar(tb_livro livro)
+        {
+            if (ModelState.IsValid)
+            {
+                DadosEntities context = new DadosEntities();
+                context.tb_livro.Add(livro);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(livro);
+        }
+
+        public ActionResult Editar(int id)
+        {
+            DadosEntities context = new DadosEntities();
+            return View(context.tb_livro.First(l => l.id == id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Editar(tb_livro livro)
+        {
+            if (ModelState.IsValid)
+            {
+                DadosEntities context = new DadosEntities();
+                tb_livro li = context.tb_livro.First(l => l.id == livro.id);
+                li.nome = livro.nome;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(livro);
+        }
+
+        public ActionResult Detalhe(int id)
+        {
+            return View(new DadosEntities().tb_livro.First(l => l.id == id));
+        }
+
+        public ActionResult Excluir(int id)
+        {
+            return View(new DadosEntities().tb_livro.First(l => l.id == id));
+        }
+
+        [HttpPost, ActionName("Excluir")]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult ConfirmaExcluir(int id)
+        {
+            DadosEntities context = new DadosEntities();
+            tb_livro livro = context.tb_livro.First(l => l.id == id);
+            context.tb_livro.Remove(livro);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
